4655555c6362c60c5971dd0d7dd4d2f344e9bf0d
DevKayoS <kayoribeirodev@gmail.com>
2025-04-16 22:18:07 -0300

    :tada: first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./foo.db")
+	if err != nil {
+		panic(err)
+	}
+
+	_ = db
+}
