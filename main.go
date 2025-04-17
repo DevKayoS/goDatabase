214de73f25d02233366bd835bdc010e0c173f807
DevKayoS <kayoribeirodev@gmail.com>
2025-04-16 23:54:36 -0300

    aprendendo a usar sqlite3

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -12,5 +13,44 @@ func main() {
 		panic(err)
 	}
 
-	_ = db
+	createTableSql := `
+		CREATE TABLE foo(
+			id integer not null primary key,
+			name text
+		);
+	`
+	res, err := db.Exec(createTableSql)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(res.RowsAffected())
+
+	insertSql := `
+		INSERT INTO foo(id, name) VALUES (1, 'teste')
+	`
+
+	res, err = db.Exec(insertSql)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(res.RowsAffected())
+
+	type user struct {
+		ID   int64
+		Name string
+	}
+
+	querySql := `
+		SELECT * FROM foo where id = ?
+	`
+
+	var u user
+	if err := db.QueryRow(querySql, 1).Scan(&u.ID, &u.Name); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(u)
+
 }
